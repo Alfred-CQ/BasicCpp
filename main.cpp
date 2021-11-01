805be28e314f8883b2412a10afc7af7d40d10244
Alfred Chillitupa <alfred.addison.cq@gmail.com>
2021-10-31 21:24:25 -0500

    Modificacion de librerias y parametros de entrada

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    Style s_1("Font-size",15);
+    Style s_1("Padding",15);
     cout << "Hola Git" << endl;
     int arr[5] = {1,2,3,4,5};
     for (int i: arr)
