4188b7478899ee7f203cc7ad1cdc4f4e69133a8c
Alfred Chillitupa <alfred.addison.cq@gmail.com>
2021-10-31 21:26:05 -0500

    Modificacion de librerias,parametros de entrada y salto de linea

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main()
 {
-    Style s_1("Font-size",15);
+    Style s_1("Index",15);
     cout << "Hola Git" << endl;
     int arr[5] = {1,2,3,4,5};
     for (int i: arr)
     	cout << i << " ";
+    cout << endl;
     return 0;
 }
 
