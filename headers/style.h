e07b9ad7437745485cf3d3627a3af98e4ed00fba
Alfred Chillitupa <alfred.addison.cq@gmail.com>
2021-12-28 16:18:05 -0500

    [Fix] dependencias de librerias y funciones cabecera

--- headers/style.h
+++ headers/style.h
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
+
+using std::string;
+
 class Style
 {
 	public:
 		Style(string*,int);
 		~Style();
+
+        void set_style();
 	private:
 		string m_style;
 		int m_parameter;
